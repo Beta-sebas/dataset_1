cb4bd32ae7dc90579d590efbd35a215b8f40a508
Andres Coral <andres.coral124@gmail.com>
2023-11-10 01:24:25 -0500

    formato del dataset ajustado a tipo JSONL (distinto de JSON), donde cada dato debe ocupar solo 1 linea. Agregados 2 scripts de utilidad para procesar el dataset

---
+++ dataset_preparation.py
@@ -0,0 +1,29 @@
+import json
+
+
+with open("dataset_1.jsonl", encoding="utf-8") as f:
+    dataset = [json.loads(line) for line in f]
+    
+    
+def show_dataset():
+        
+        print(json.dumps(dataset, indent=2))
+      
+        
+def modify_system_job():
+    
+    new_system_job = "INSERT HERE NEW SYSTEM JOB"
+    new_jsonl_name = "dataset_01_modified.jsonl"
+    
+    for data in dataset:
+        data["messages"][0]["content"] = new_system_job
+    
+    with open(new_jsonl_name, 'w', encoding="utf8") as file:
+        for data in dataset:
+            json_line = json.dumps(data)
+            file.write(json_line + '\n')
+
+
+       
+modify_system_job()
+show_dataset()
